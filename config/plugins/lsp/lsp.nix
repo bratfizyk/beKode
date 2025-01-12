dda9bd50a062418bd970de385ef99b6d235c218f
Alojzy Leszcz <aleszcz@pm.me>
2025-01-12 23:28:36 +0100

    Adding fancy diagnostic icons

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -16,4 +16,10 @@
       enable = true;
     };
   };
+  extraConfigVim = ''
+    sign define DiagnosticSignError text=✘ texthl=DiagnosticSignError linehl= numhl=
+    sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=
+    sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo linehl= numhl=
+    sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=
+  '';
 }
